a34351a8b10990667b53b0a102fd6c0a84cd9c2e
Sam Williams <samwilliams281@gmail.com>
2022-03-20 09:39:01 -0400

    finish html generation

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-// const pageHTML = generatePage(name, github);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 
   const promptUser = () => {
       return inquirer.prompt([
@@ -140,5 +134,11 @@ const inquirer = require('inquirer');
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+      const pageHTML = generatePage(portfolioData);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+});
   });
